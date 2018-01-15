f92131fb01de94f7f787ae9ea538380cb27f7dc8
Fred <kyush.spr@gmail.com>
2018-01-15 15:17:18 +0100

    Add thumbnail component

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Thumbnail extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            url: "https://img.youtube.com/vi/"+ this.props.vid +"/0.jpg"
+        }
+    }
+
+    render() {
+      return (
+        <div className="Thumbnail">
+            <img src={this.state.url} alt="thumbnail" width="150" height="150"/>
+            <div>Blabla</div>
+        </div>
+      );
+    }
+}
+  
+export default Thumbnail;
