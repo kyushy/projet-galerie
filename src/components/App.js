4f69a1138c3de29ed082b200afe3a124679a19b1
Fred <kyush.spr@gmail.com>
2018-01-22 12:20:30 +0100

    add nav bar to video component

--- src/components/App.js
+++ src/components/App.js
@@ -29,8 +29,8 @@ class App extends Component {
 
   showVideo(key){
     console.log(key);
-    /*ReactDOM.render(<Video video={key} />, 
-      document.getElementById('root'));*/
+    ReactDOM.render(<Video video={key} />, 
+      document.getElementById('root'));
   }
 
   render() {
@@ -40,10 +40,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
+        {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Galerie MBDS</h1>
-        </header>
+        </header>*/}
         <div className="container">
           <div className="row">{thumbnails}</div>
         </div>
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import App from './App.js';
 
 class Video extends Component {
     constructor(props){
@@ -9,14 +10,21 @@ class Video extends Component {
     }
 
     render() {
+
       return (
-        <div>
-            <div>{this.props.video.titre}</div>
-            <iframe width="560" height="315" 
-                src={this.state.url} 
-                frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
-            </iframe>
-            <div>{this.props.video.desc}</div>
+        <div className="Video">
+            <div id="frame">
+                <button type="button" onClick={() => this.props.BackWasClicked()}>Retour index</button>
+                <div>{this.props.video.titre}</div>
+                <iframe width="560" height="315" title="my video"
+                    src={this.state.url} 
+                    frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
+                </iframe>
+                <div>{this.props.video.desc}</div>
+            </div>
+            <div id="nav">
+                <App />
+            </div>
         </div>
       );
     }
