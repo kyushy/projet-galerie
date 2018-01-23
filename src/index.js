510bf63cbf37d55d2782281ae1c3b520521433a7
Fred <kyush.spr@gmail.com>
2018-01-23 12:54:00 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch
+  } from 'react-router-dom'
 import './css/index.css';
 import App from './components/App';
+import Video from './components/Video';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+      <Router>
+        <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/videos/:video" component={Video}/>
+                <Route component={NoMatch}/>
+        </Switch>
+      </Router>
+)
+
+const NoMatch = ({ location }) => (
+    <div>
+      <h3>No match for <code>{location.pathname}</code></h3>
+    </div>
+)
+  
+ReactDOM.render(<Root/>, document.querySelector('#root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
